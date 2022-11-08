85787ab634d49a69e2ef9d0f2cb3c48d86d5e968
Michael Gostomski <gostomski.m@gmail.com>
2022-11-08 12:44:35 -0600

    routes added for user and game

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-// const gameRoutes = require('./gameRoutes');
+gameRoutes = require('./gameRoutes');
 
-// router.use('/game', gameRoutes);
+router.use('/game', gameRoutes);
 router.use('/users', userRoutes);
 
 module.exports = router;
--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,6 +1,16 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
+router.get('/', async (req, res) => {
+  try {
+    const userData = await User.findByPk(req.params.id);
+    const user = userData.get(({ plain: true}));
+    res.render('user');
+  } catch (err) {
+    res.status(404).json({ message: 'No user with this id.'});
+  }
+})
+
 router.post('/', async (req, res) => {
     console.log(req.body);
   try {
@@ -13,6 +23,17 @@ router.post('/', async (req, res) => {
     });
   } catch (err) {
     res.status(400).json(err);
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  try {
+    const newUserDeath = await User.update(
+      deaths++
+    );
+
+  } catch (err) {
+    res.status(404).json(err);
   }
 });
 
