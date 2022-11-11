008387062ebfcaf88925341ae3af1940916a54aa
Michael Gostomski <gostomski.m@gmail.com>
2022-11-10 21:42:06 -0600

    combined victory view into continue view

--- controllers/api/gameRoutes.js
+++ controllers/api/gameRoutes.js
@@ -1,6 +1,5 @@
 const router = require('express').Router();
-const { Prompt } = require('../../models');
-const { Message } = require('../../models');
+const { Prompt, Message } = require('../../models');
 
 // GET route for getting a game prompt
 router.get('/:id', async (req, res) => {
@@ -21,29 +20,15 @@ router.get('/:id', async (req, res) => {
 // GET route for continue game screen
 router.get('/continue/:id', async (req, res) => {
     try {
-        const promptData = await Prompt.findbyPk(req.params.id);
+        const promptData = await Prompt.findByPk(req.params.id);
+        // console.log(promptData);
         if (!promptData) {
             res.status(404).json({ message: 'You might have wandered off the path' });
         }
         const prompts = promptData.get({ plain: true });
         res.render('continue', prompts);
     } catch (err) {
-        console.log(err);
-        res.status(500).json(err);
-    };
-});
-
-// GET route for startover screen
-router.get('/startover', async (req, res) => {
-    try {
-        const gameData = await Message.findAll({
-            where: {
-                message_type: "gameover"
-            },
-        });
-        const gameOver = gameData.get({ plain: true });
-        res.render('startover', gameOver);
-    } catch (err) {
+        // console.log(err);
         res.status(500).json(err);
     };
 });
@@ -58,21 +43,6 @@ router.get('/victory', async (req, res) => {
         });
         const gameWin = gameData.get({ plain: true });
         res.render('win page', gameWin);
-    } catch (err) {
-        res.status(500).json(err);
-    };
-});
-
-// GET route for death screen
-router.get('/death', async (req, res) => {
-    try {
-        const gameData = await Message.findAll({
-            where: {
-                message_type: "gameover"
-            },
-        });
-        const gameWin = gameData.get({ plain: true });
-        res.render('deathpage', gameWin);
     } catch (err) {
         res.status(500).json(err);
     };
